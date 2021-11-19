f3e7f22655d3b8de6f7e09df4fb345c5c4213f74
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2021-11-19 15:05:46 +0300

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
+    backgroundImage: {
+      'uzaktan-calisma': "url('/slider_bg/uzaktan-calisma.png')",
+      'bluuty-slider': "url('/slider_bg/bluuty-slider.jpeg')",
+      'el-birligi-dijital-donusum': "url('/slider_bg/ElBirligi_DijitalDonusumKredisi.jpeg')",
+      'ssl-vpn': "url('/slider_bg/ssl-vpn.png')",
+      'bulut-santral-cozumleri': "url('/slider_bg/bulut-santral-cozumleri-isnet.png')",
+      'e-donusum-min': "url('/slider_bg/e-donusum-min.png')",
+      'uydu-erisim': "url('/slider_bg/uydu-erisimi.png')",
+      'isnet-veeam-cloud': "url('/slider_bg/isnet-veeam-cloud-connect-slider.png')",
+      'robotik-surec': "url('/slider_bg/robotik-surec-otomasyonu-isnet.png')",
+      'veri-guvenligi': "url('/slider_bg/veri-guvenliginizi-kvkk-cozumlerimizle-saglayin.jpeg')"
+    },
+    extend: {
+      height: {
+        slider: '600px',
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat']
+      }
+    },
   },
   variants: {
     extend: {},
